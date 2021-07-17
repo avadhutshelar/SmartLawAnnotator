bfc0ba7966ea15260e97c03ba8ce4b3f5eace6ee
Avadhut Shelar <avadhut.shelar@gmail.com>
2021-07-17 13:19:17 +0530

    changes

--- src/main/java/in/edu/rvce/slanno/controllers/ProjectPreProcessDocumentsController.java
+++ src/main/java/in/edu/rvce/slanno/controllers/ProjectPreProcessDocumentsController.java
@@ -2,19 +2,13 @@ package in.edu.rvce.slanno.controllers;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
 import in.edu.rvce.slanno.entities.LegalDocument;
