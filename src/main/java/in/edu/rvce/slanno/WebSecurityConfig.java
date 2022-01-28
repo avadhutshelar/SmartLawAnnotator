5b4e7bd8be0e570dcbfb15a261b327d60daed0fd
avadhutshelar <avadhutshelar@users.noreply.github.com>
2022-01-28 10:24:43 +0530

    SLANNO-17 H2 DB user authentication initial commit

--- src/main/java/in/edu/rvce/slanno/WebSecurityConfig.java
+++ src/main/java/in/edu/rvce/slanno/WebSecurityConfig.java
@@ -1,30 +1,45 @@
 package in.edu.rvce.slanno;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+
+		http.authorizeRequests()
+			.antMatchers("/systemSettings").hasRole("ADMIN")
+			.antMatchers("/project/**/annotate/**").hasAnyRole("ANNOTATOR", "ADMIN")
+			.antMatchers("/").permitAll().and().formLogin()
+			.loginPage("/login").permitAll().and().logout().permitAll();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 
 	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder().username("user1").password("user1").roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
